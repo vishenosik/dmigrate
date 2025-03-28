e683dbc158ed6dfc72342396d95d66d78a7eabb7
vishenosik <vishenosik@github.com>
2025-03-28 11:02:38 +0400

    test(docker tests): Add more time to sleep waiting for docker.

--- scripts/testing.sh
+++ scripts/testing.sh
@@ -6,7 +6,7 @@ TESTING_LIST=$(go list ./... | grep -v mocks | grep -v gen )
 docker compose -f ./test/compose/docker-compose.yml up --force-recreate --remove-orphans --detach
 
 echo waiting for docker...
-sleep 10
+sleep 15
 echo done waiting for docker, start testing...
 
 go test -v -cover -coverprofile=$COVERAGE_FILE $TESTING_LIST
